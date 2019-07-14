25c3d545cb9504367bf0273a27008b19597f707d
Gorm Casper <g@g.dk>
2019-07-14 16:32:27 +0200

    change port and comment in the needed config for a prod run

--- config/prod.secret.exs
+++ config/prod.secret.exs
@@ -32,7 +32,7 @@ config :jazz, JazzWeb.Endpoint,
 # If you are doing OTP releases, you need to instruct Phoenix
 # to start each relevant endpoint:
 #
-#     config :jazz, JazzWeb.Endpoint, server: true
+config :jazz, JazzWeb.Endpoint, server: true
 #
 # Then you can assemble a release by calling `mix release`.
 # See `mix help release` for more information.
