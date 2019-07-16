bdd894bf92f2f17f4756325e26a2a90bc66eb03c
Gorm Casper <git@mail.gormcasper.dk>
2019-07-16 22:42:08 +0200

    getter attempts to get more content out of entries than feeder_ex can do

--- lib/rss/get.ex
+++ lib/rss/get.ex
@@ -1,7 +1,10 @@
 defmodule RSS do
+  @content_left ~r/^(.*?)<content:encoded><!\[CDATA\[/
+  @content_right ~r/]]><\/content\:encoded>/
+
   def get(url) do
     case fetch_(url) do
-      {:ok, feed} -> parse(url, feed)
+      {:ok, feed, contents} -> parse(url, feed, contents)
       _ -> {:error, "Could not fetch or parse feed"}
     end
   end
@@ -9,11 +12,20 @@ defmodule RSS do
   defp fetch_(url) do
     with {:ok, %HTTPoison.Response{body: body}} <- HTTPoison.get(url),
          {:ok, feed, _header} <- FeederEx.parse(body),
-         do: {:ok, feed}
+         contents <- find_content(body),
+         do: {:ok, feed, contents}
   end
 
-  defp parse(url, feed) do
-    case parse_posts(feed.title, feed.entries) do
+  defp parse(url, feed, contents) do
+    entries = if length(feed.entries) == length(contents) do
+      feed.entries
+      |> Stream.zip(contents)
+      |> Enum.map(fn {entry, content} -> Map.put(entry, :summary, content) end)
+    else
+      feed.entries
+    end
+
+    case parse_posts(feed.title, entries) do
       {:ok, posts} ->
         {:ok, %{title: feed.title, url: url}, posts}
       _ ->
@@ -34,7 +46,7 @@ defmodule RSS do
       end)
     }
   end
-    
+
   defp parse_post(feed_title, entry) do
     %{
       guid: entry.id || guid_from_title(entry.title),
@@ -49,5 +61,25 @@ defmodule RSS do
     title
     |> Base.encode16(case: :lower)
     |> String.slice(0, 20)
+  end
+
+  defp find_content(text) do
+    text = String.replace(text, "\n", " ")
+    process_content(text, [], :to_left)
+  end
+
+  defp process_content(text, contents, :to_left) do
+    # We remove until @content_left
+    text = String.replace(text, @content_left, "")
+    process_content(text, contents, :to_right)
+  end
+
+  defp process_content(text, contents, :to_right) do
+    case String.split(text, @content_right, parts: 2) do
+      [_rest] -> contents # done
+      [content, text] ->
+        contents = [String.trim(content) | contents]
+        process_content(text, contents, :to_left)
+    end
   end
 end
